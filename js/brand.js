b7ebb8596c0e140d0f58a0ffe4991ae375b46e05
Amsssv <Amsssv@yandex.ru>
2022-08-22 15:32:59 +0300

    brand refactor

---
+++ js/brand.js
@@ -0,0 +1,18 @@
+$('.brand__content').owlCarousel({
+	nav: true,
+	dots: false,
+	responsive: {
+		0: {
+			items: 2
+		},
+		550: {
+			items: 3
+		},
+		1000: {
+			items: 4
+		},
+		1200: {
+			items: 5
+		}
+	}
+})
---
+++ js/services.js
@@ -0,0 +1,18 @@
+$('.services__content').owlCarousel({
+	nav: false,
+	dots: false,
+	responsive: {
+		0: {
+			items: 1
+		},
+		500: {
+			items: 2
+		},
+		767: {
+			items: 3
+		},
+		1200: {
+			items: 4
+		}
+	}
+})
